bf537214c85247ba3a80c0c415e4ae7f63978c3a
aiXpertLab <aiXpertLab@gmail.com>
2024-04-22 16:52:39 -0400

    fastai

--- Aritifical_Intelligence.py
+++ Aritifical_Intelligence.py
@@ -3,4 +3,8 @@ import streamlit as st
 from utils.st_def import st_main_contents, st_logo
 
 st_logo(title='👋 Artificial Intelligence! 🍨 ', page_title="AI🍨",)
-st_main_contents()
+tab1, tab2, tab3, tab4, tab5 = st.tabs(["General", "Learning Material", "Prediction", "Train NN","Conclusion"])
+
+with tab1: st_main_contents()
+with tab2:
+    st.markdown('[FastAI Deep Learning](https://course.fast.ai/)',unsafe_allow_html=True)
