72bd88a7f00d2f1afd258be305650029a7a35c6d
MERNTS <ucrossgram@gmail.com>
2024-06-02 17:41:51 -0400

    v1

--- Artifical_Intelligence_1950.py
+++ Artifical_Intelligence_1950.py
@@ -1,28 +1,13 @@
 # from hypecheth
 import streamlit as st
-from utils.st_def import st_main_contents, st_logo
+from utils import st_def, tab_home
 
-st_logo(title='👋 Artificial Intelligence! 🍨 ', page_title="AI🍨",)
+st_def.st_logo(title='👋 Artificial Intelligence! 🍨 ', page_title="AI🍨",)
 tab1, tab2, tab3, tab4, tab5 = st.tabs(["General", "Learning ", "Developing Environment", "Developing Process",""])
 
-with tab1: st_main_contents()
-with tab2:
-    st.markdown('[FastAI Deep Learning](https://course.fast.ai/)',unsafe_allow_html=True)
-with tab3:
-    st.markdown('### Python')
-    import scipy, numpy, matplotlib, pandas, statsmodels, sklearn, PIL
-    st.text('scipy: %s' % scipy.__version__)
-    st.text('numpy: %s' % numpy.__version__)
-    st.text('matplotlib: %s' % matplotlib.__version__)
-    st.text('pandas: %s' % pandas.__version__)
-    st.text('statsmodels: %s' % statsmodels.__version__)
-    st.text('sklearn: %s' % sklearn.__version__)
-    st.text(f'PIL: {PIL.__version__}' )
-    
-    st.markdown('### Deep Learning')
-    import tensorflow, keras
-    st.text('TF: %s' % tensorflow.__version__)
-    st.text('Keras: %s' % keras.__version__)
+with tab1: tab_home.home_main_contents()
+with tab2: st.markdown('[FastAI Deep Learning](https://course.fast.ai/)',unsafe_allow_html=True)
+with tab3: 
     
     
 with tab4: 
--- pages/7_🫕Case 5 - FineTune.py
+++ pages/7_🫕Case 5 - FineTune.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_extras.stateful_button import button
-from utils import st_def, tab_dogs_cats
+from utils import st_def
 
 st_def.st_logo(title = "👋Dogs and Cats", page_title="Summary",)
 
@@ -8,15 +8,55 @@ t1, t2, t3, t4, t5, t6 = st.tabs(["Llama3", " ", " ", '', '',''])
 
 with t1: 
     # https://www.confident-ai.com/blog/the-ultimate-guide-to-fine-tune-llama-2-with-llm-evaluations
-    st.subhead('Step 1—Installation')
+    st.write('The way everyone should be thinking about fine-tuning, is not how we can outperform OpenAI or replace RAG, but how we can maintain the same performance while cutting down on inference time and cost for your specific use case.')
+    st.write('''Fine-tuning comes in two different forms:
+             
+            - SFT (Supervised Fine-Tuning): LLMs are fine-tuned on a set of instructions and responses. The model’s weights will be updated to minimize the difference between the generated output and labeled responses.
+            - RLHF (Reinforcement Learning from Human Feedback): LLMs are trained to maximize the reward function (using Proximal Policy Optimization Algorithms or the Direct Preference Optimization (DPO) algorithm). This technique uses feedback from human evaluation of generated outputs, which in turn captures more intricate human preferences, but is prone to inconsistent human feedback.
+             
+             ''')
+    st.subheader('Step 1—Installation')
     st.write("""
         - transformers: to load models, tokenizers, etc.
         - peft: to perform parameter efficient fine-tuning
         - bitsandbytes: to setup 4-bit quantization
         - trl: for supervised fine-tuning
         - deepeval: to evaluate the fine-tuned LLM
-    
-    
-    
     """)
     
+    st.subheader('Step 2— Quantization Setup')
+    st.write('''
+             To optimize Colab RAM usage during LLaMA-3 8B fine-tuning, we use QLoRA (quantized low-rank approximation). Here’s a breakdown of its key principles:
+
+- 4-Bit Quantization: QLoRA compresses the pre-trained LLaMA-3 8B model by representing weights with only 4 bits (as opposed to standard 32-bit floating-point). This significantly shrinks the model’s memory footprint.
+- Frozen Pre-trained Model: After quantization, the vast majority of LLaMA-3’s parameters are frozen. This prevents direct updates to the core model during fine-tuning.
+- Low-Rank Adapters: QLoRA introduces lightweight, trainable adapter layers into the model’s architecture. These adapters capture task-specific knowledge without drastically increasing the number of parameters.
+- Gradient-Based Fine-tuning: During the fine-tuning process, gradients flow through the frozen 4-bit quantized model but are used to update solely the parameters within the low-rank adapters. This isolated optimization greatly reduces computational overhead.
+             ''')
+    
+    st.subheader('Step 3 — Load LLaMA-3 with QLoRA Configuration')
+    st.write('''
+             
+             
+                 ''')
+
+
+
+    st.write('''
+             
+             
+                 ''')
+
+
+
+    st.write('''
+             
+             
+                 ''')
+
+
+
+    st.write('''
+             
+             
+                 ''')
