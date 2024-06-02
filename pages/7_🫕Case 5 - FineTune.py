9294555abad46494867e80026917239d82afc1d3
aiXpertLab <aiXpertLab@gmail.com>
2024-06-02 16:32:32 -0400

    update

--- pages/7_🫕Case 5 - FineTune.py
+++ pages/7_🫕Case 5 - FineTune.py
@@ -4,9 +4,19 @@ from utils import st_def, tab_dogs_cats
 
 st_def.st_logo(title = "👋Dogs and Cats", page_title="Summary",)
 
-t1, t2, t3, t4, t5, t6 = st.tabs(["Preprocessing", "Small Dataset", "VGG16 Feature Extracting", 'VGG16 Fine Tuning', '',''])
+t1, t2, t3, t4, t5, t6 = st.tabs(["Llama3", " ", " ", '', '',''])
 
-with t1: tab_dogs_cats.dc_preprocessing()
-with t2: tab_dogs_cats.dc_small_dataset()
-with t3: tab_dogs_cats.dc_vgg16_feature_extracting()
-with t4: tab_dogs_cats.dc_vgg16_fine_tuning()
+with t1: 
+    # https://www.confident-ai.com/blog/the-ultimate-guide-to-fine-tune-llama-2-with-llm-evaluations
+    st.subhead('Step 1—Installation')
+    st.write("""
+        - transformers: to load models, tokenizers, etc.
+        - peft: to perform parameter efficient fine-tuning
+        - bitsandbytes: to setup 4-bit quantization
+        - trl: for supervised fine-tuning
+        - deepeval: to evaluate the fine-tuned LLM
+    
+    
+    
+    """)
+    
